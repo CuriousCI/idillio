3b96ca749df267895e421ea397eca945c6a41c8e
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-05-13 22:17:47 +0200

    Tooltip

---
+++ src/lib/tooltip/Tooltip.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Align } from '$lib/util';
+
+	export let text: string = '',
+		justify: Align = Align.CENTER,
+		align: Align = Align.END;
+
+	let show: boolean = false;
+</script>
+
+<span
+	on:focus={() => {}}
+	on:blur={() => {}}
+	on:mouseover={() => (show = true)}
+	on:mouseout={() => (show = false)}
+	class="relative"
+>
+	<slot />
+	{#if show}
+		<span
+			class:top-full={align == Align.END}
+			class:bottom-full={align == Align.START}
+			class="z-10 whitespace-nowrap absolute w-min m-1 left-1/2 -translate-x-1/2 py-2 px-4 rounded-lg bg-b-03 text-c-1"
+		>
+			{text}
+		</span>
+	{/if}
+</span>
+
+<!-- #474747 -->
+
+<!-- 
+class:top-full={align == Align.END}
+class:bottom-full={align == Align.START}
+class:left-full={justify == Align.END}
+class:right-full={justify == Align.START} 
+-->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Input from '$lib/input/Input.svelte';
+	import Tooltip from '$lib/tooltip/Tooltip.svelte';
+	import { Align } from '$lib/util';
 </script>
 
 <h1 class="font-bold text-9xl">Idillio</h1>
@@ -7,18 +9,22 @@
 <p>This should be the documentation! Coming soon</p>
 <br />
 
-<form action="">
-	<Input
-		name="name"
-		label="Name"
-		pattern={/^A.*/}
-		warningMessage="Avvertenza a caso"
-		errorMessage="Deve cominciare con 'A'"
-		normalize={(input) => input.toUpperCase()}
-		placeholder="Nome tutto maisucolo"
-		suggestion="Hi!"
-		required
-	/>
-	<br />
-	<input type="submit" value="submit" />
-</form>
+<!-- <Tooltip text="Bella, come stai?">ciao!</Tooltip> -->
+
+<Tooltip text="Form...">
+	<form action="" class="w-min">
+		<Input
+			name="name"
+			label="Name"
+			pattern={/^A.*/}
+			warningMessage="Avvertenza a caso"
+			errorMessage="Deve cominciare con 'A'"
+			normalize={(input) => input.toUpperCase()}
+			placeholder="Nome tutto maisucolo"
+			suggestion="Hi!"
+			required
+		/>
+		<br />
+		<input type="submit" value="submit" />
+	</form>
+</Tooltip>
