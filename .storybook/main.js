ad12988bc7d917ac88ed61ca8d2ae4a9fd7da99a
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-07-18 10:05:30 +0200

    Fix packaging

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,21 @@
 module.exports = {
-	"stories": [
-		"../src/**/*.stories.mdx",
-		"../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+	'stories': [
+		'../src/**/*.stories.mdx',
+		'../src/**/*.stories.@(js|jsx|ts|tsx|svelte|mdx)'
 	],
-	"addons": [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions"
+	'addons': [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions'
 	],
-	"framework": "@storybook/svelte",
-	"core": {
-		"builder": "@storybook/builder-vite"
+	'framework': '@storybook/svelte',
+	'core': {
+		'builder': '@storybook/builder-vite'
 	},
-	"svelteOptions": {
-		"preprocess": import("../svelte.config.js").preprocess
+	'svelteOptions': {
+		'preprocess': import('../svelte.config.js').preprocess
 	},
-	"features": {
-		"storyStoreV7": true
+	'features': {
+		'storyStoreV7': true
 	}
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = 'production' === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({ postcss: true }),
 	kit: {
 		adapter: adapter({ pages: 'docs', assets: 'docs', fallback: true }),
+		package: {
+			files(filepath) {
+				return !filepath.endsWith('.stories.svelte')
+			}
+		},
 		paths: {
 			// change below to your repo name
-			base: dev ? '' : "/idillio",
+			base: '/idillio',
 		},
 		trailingSlash: 'always',
 		prerender: { default: true },
